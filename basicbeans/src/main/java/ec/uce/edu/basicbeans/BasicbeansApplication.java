de6df38577352ddc73bbd65cea755c2a4d7060df
XavierT1 <chavi.jtr10@gmail.com>
2024-05-15 08:46:51 -0500

    GalagaGame SpringBoot

--- basicbeans/src/main/java/ec/uce/edu/basicbeans/BasicbeansApplication.java
+++ basicbeans/src/main/java/ec/uce/edu/basicbeans/BasicbeansApplication.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
+@SpringBootApplication
 @ComponentScan(basePackages = {"ec.uce.edu.models", "ec.uce.edu.basicbeans"})
 public class BasicbeansApplication {
 
